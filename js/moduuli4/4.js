a9985fd2a80e103e108dbf8abddaad93415b5578
zbr12 <105425093+zbr12@users.noreply.github.com>
2023-04-14 13:34:00 +0300

    ttehtävoä

--- js/moduuli4/4.js
+++ js/moduuli4/4.js
@@ -12,10 +12,12 @@ haku.addEventListener('submit', async function(evt) {
         const response = await fetch(`https://api.tvmaze.com/search/shows?q=${nimi}`);    // starting data download, fetch returns a promise which contains an object of type 'response'
         const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
         for (let i =0; i<jsonData.length; i++) {
-            let nimi = jsonData[i].show.name;
-            let linkki = jsonData[i].show.url;
-            let kuva = jsonData[i].show.image?.medium;
-            let yhteenveto = jsonData[i].show.summary;
+            console.log(jsonData[i]);
+            let ohjelma = jsonData[i];
+            let nimi = ohjelma.show.name;
+            let linkki = ohjelma.show.url;
+            let kuva = ohjelma.show.image && ohjelma.show.image.medium ? ohjelma.show.image.medium : 'https://via.placeholder.com/210x295?text=Not%20Found';
+            let yhteenveto = ohjelma.show.summary;
 
             let h2 = document.createElement('h2'); 
             let t = document.createTextNode(nimi);  // create text node
